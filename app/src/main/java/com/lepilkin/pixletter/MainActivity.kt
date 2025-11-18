e0adf16c548368fa2daeb67da6bf76c2568de01f
Лепилкин Максим <maksimlepilkin7@gmail.com>
2025-11-18 22:42:02 +0300

    all2

--- app/src/main/java/com/lepilkin/pixletter/MainActivity.kt
+++ app/src/main/java/com/lepilkin/pixletter/MainActivity.kt
@@ -5,14 +5,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +35,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PixLetterTheme {
-                GreetingText(message = "Royal Decree!\nDear Max,\nHis Majesty the King\nInvites you to a great feast!\nCome to the castle before sunset,\notherwise, you risk losing your head!", from = "From King Arthur", modifier = Modifier.padding(8.dp))
+                GreetingImage(message = "Dear Max,\nHis Majesty the King\nInvites you to a great feast!\nCome to the castle before sunset,\notherwise, you risk losing your head!", from = "From King Arthur", modifier = Modifier.padding(8.dp))
                 }
             }
         }
@@ -36,30 +46,70 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 
-
+@Composable
+fun InvitationTitle(modifier: Modifier = Modifier){
+    Text(
+        text = stringResource(R.string.invitation_title),
+        fontSize = 28.sp,
+        color = Color.Magenta,
+        textAlign = TextAlign.Center,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp)
+    )
+}
 @Composable
 fun RoyaInvitationPreview() {
 
     PixLetterTheme {
-        GreetingText(message = "Royal Decree!\nDear Max,\nHis Majesty the King\nInvites you to a great feast!\nCome to the castle before sunset,\notherwise, you risk losing your head!", from = "From King Arthur")
+        GreetingText(message = stringResource(R.string.king_message_text), from = stringResource(R.string.from_king_arthur), )
 
+    }
+}
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.pixelcasstle)
+    Box(modifier){
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            alpha = 0.8F,
+            modifier = Modifier.fillMaxSize(),
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
     }
 }
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Column (
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Bottom,
         modifier = modifier
+            .border(
+                width = 2.dp,
+                color = Color.White,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding( 16.dp)
     ) {
+        InvitationTitle()
         Text(
             text = message,
             fontSize = 20.sp,
             lineHeight = 40.sp,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            color = Color.White
         )
         Text(
             text = from,
             fontSize = 24.sp,
+            color = Color.White,
             modifier = Modifier
                 .padding(16.dp)
                 .align(alignment = Alignment.End)
