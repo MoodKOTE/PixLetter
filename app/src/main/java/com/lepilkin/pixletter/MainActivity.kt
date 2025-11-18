a4b024afd4207fc70bc64199f493165204c41fbe
Лепилкин Максим <maksimlepilkin7@gmail.com>
2025-11-18 17:03:09 +0300

    all1

--- app/src/main/java/com/lepilkin/pixletter/MainActivity.kt
+++ app/src/main/java/com/lepilkin/pixletter/MainActivity.kt
@@ -5,16 +5,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.lepilkin.pixletter.ui.theme.PixLetterTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,31 +25,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PixLetterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                GreetingText(message = "Royal Decree!\nDear Max,\nHis Majesty the King\nInvites you to a great feast!\nCome to the castle before sunset,\notherwise, you risk losing your head!", from = "From King Arthur", modifier = Modifier.padding(8.dp))
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Gray) {
-        Text(
-            text = "Hi, my name is $name!",
-            modifier = modifier.padding(24.dp)
-        )
-    }
-}
+
+
 
 @Preview(showBackground = true)
+
+
 @Composable
-fun GreetingPreview() {
+fun RoyaInvitationPreview() {
+
     PixLetterTheme {
-        Greeting("Max")
+        GreetingText(message = "Royal Decree!\nDear Max,\nHis Majesty the King\nInvites you to a great feast!\nCome to the castle before sunset,\notherwise, you risk losing your head!", from = "From King Arthur")
+
+    }
+}
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 20.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
     }
 }
